a0832cc244c4baa5782f7f8d93dc3a5fef47b604
LiveUser <secured_identity@protonmail.com>
2022-03-12 20:12:13 -0400

    First Gitted Version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.0.1+3 - October 9 2021
+* Added null safety
+
+## 0.0.1+2 - January 15 2021
+* changed the cupertino import to import 'package:meta/meta.dart'; for the use of the @required keyword
+
 ## 0.0.1 - December 17 2020
 
 * Added my super trolled library functions
--- lib/caesar_salad.dart
+++ lib/caesar_salad.dart
@@ -3,7 +3,10 @@ library caesar_salad;
 // ignore: camel_case_types
 class caesar_salad {
   ///Encryption method
-  static List<int> make({List<int> data,int password}){
+  static List<int> make({
+      required List<int> data,
+      required int password,
+    }){
     List<int> encrypted = [];
     for(int i = 0; i < data.length; i++){
       encrypted.add(data[i] + password);
@@ -11,7 +14,10 @@ class caesar_salad {
     return encrypted;
   }
   ///Decryption method
-  static List<int> compost({List<int> data,int password}){
+  static List<int> compost({
+      required List<int> data,
+      required int password,
+    }){
     List<int> decrypted = [];
     for(int i = 0; i < data.length; i++){
       decrypted.add(data[i] - password);
